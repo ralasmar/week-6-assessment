e986f778dadb131a78cb7e4ef958f44867728536
Rola Alasmar <alasm002@umn.edu>
2023-05-14 19:27:29 -0500

    week-6-assessment

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -12,13 +12,13 @@ afterEach(async () => {
 
 describe("Duel Duo tests", () => {
   test("page loads with title", async () => {
-    await driver.get("http://localhost:8000");
+    await driver.get("http://3.23.132.44:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
 
   test('clicking draw displays the div with id=choices', async () => {
     //navigate to the app
-    await driver.get("http://localhost:8000")
+    await driver.get("http://3.23.132.44:8000")
     //find and select the draw button
     await driver.findElement(By.css('button[id="draw"]')).click()
     //wait to see if it displays the div with id=choices
@@ -27,7 +27,7 @@ describe("Duel Duo tests", () => {
 
   test('clicking see all bots displays the div with id=all-bots', async () => {
     //navigate to the app 
-    await driver.get("http://localhost:8000")
+    await driver.get("http://3.23.132.44:8000")
     //find and select the add to duo button
     await driver.findElement(By.css('button[id="see-all"]'))
     //wait to see if it displays the div with id=see-all
